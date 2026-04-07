fcd375190ee68017357710825f4f25a55a41afbc
김재원 <dumdum7@hs.ac.kr>
2026-04-07 14:09:38 +0900

    Refactor : 메소드명 변경

--- src/main/java/com/eduquest/backend/presentation/identity/controller/UserController.java
+++ src/main/java/com/eduquest/backend/presentation/identity/controller/UserController.java
@@ -23,7 +23,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -65,7 +64,7 @@ public class UserController {
     }
 
     @GetMapping("/users/{uuid}")
-    public ResponseEntity<UserProfileResponse> getUser(@PathVariable UUID uuid) {
+    public ResponseEntity<UserProfileResponse> getUserProfile(@PathVariable UUID uuid) {
 
         UserProfileCommand.ProfileResponse profileResponse = userProfileService.getUserProfile(uuid);
 
