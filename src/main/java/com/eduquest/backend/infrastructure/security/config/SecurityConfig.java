146f38e84dce70417b2c1e87cd1d4ba9a896c81d
kimdotcom2 <105586594+kimdotcom2@users.noreply.github.com>
2026-04-29 21:35:49 +0900

    Revert "Fix : get api로 변경"

--- src/main/java/com/eduquest/backend/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/com/eduquest/backend/infrastructure/security/config/SecurityConfig.java
@@ -68,8 +68,7 @@ public class SecurityConfig {
                         "/api/v1/auth/find-password",
                         "/api/v1/auth/reset-password",
                         "/api/v1/auth/refresh",
-                        "/api/v1/auth/sign-up/*",
-                        "/api/v1/auth/sign-up/**"
+                        "/api/v1/auth/sign-up/*"
 
                 )
                     .permitAll()
