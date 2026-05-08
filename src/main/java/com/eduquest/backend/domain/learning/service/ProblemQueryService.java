9193bc5247ae2a32ea422b5cfabb0a5cea106f7d
김재원 <dumdum7@hs.ac.kr>
2026-05-08 21:35:01 +0900

    Refactor : 불필요한 메소드 정리

--- src/main/java/com/eduquest/backend/domain/learning/service/ProblemQueryService.java
+++ src/main/java/com/eduquest/backend/domain/learning/service/ProblemQueryService.java
@@ -14,10 +14,6 @@ public interface ProblemQueryService {
 
     ProblemQuery.HintDetail findHintByProblemUuidAndLevel(UUID uuid, Integer level);
 
-    Long findHintIdByProblemUuidAndLevel(UUID problemUuid, int level);
-
-    List<ProblemQuery.Summary> findAllByStageNumber(Integer stageNumber);
-
     List<ProblemQuery.Detail> findAllDetailsByStageNumber(Integer stageNumber);
 
 }
