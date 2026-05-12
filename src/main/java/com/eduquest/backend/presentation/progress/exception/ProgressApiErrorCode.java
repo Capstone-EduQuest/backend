017c61fb3ce21cab5f5c2ed68b05a09f4d588582
김재원 <dumdum7@hs.ac.kr>
2026-05-12 15:16:40 +0900

    Refactor : 불필요한 에러코드 삭제

--- src/main/java/com/eduquest/backend/presentation/progress/exception/ProgressApiErrorCode.java
+++ src/main/java/com/eduquest/backend/presentation/progress/exception/ProgressApiErrorCode.java
@@ -9,8 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ProgressApiErrorCode implements ErrorCode {
 
-    PROGRESS_NOT_ALLOWED(HttpStatus.FORBIDDEN, "진행 정보를 조회할 권한이 없습니다."),
-    PROGRESS_NOT_FOUND(HttpStatus.NOT_FOUND, "진행 정보를 찾을 수 없습니다.");
+    PROGRESS_NOT_ALLOWED(HttpStatus.FORBIDDEN, "진행 정보를 조회할 권한이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
