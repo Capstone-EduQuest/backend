8dbdcfd8dcc0ae545a839fcf6ceefaeae23a0b46
김재원 <dumdum7@hs.ac.kr>
2026-05-12 15:32:06 +0900

    Feat : 권한 조회 로직 추가

---
+++ src/main/java/com/eduquest/backend/application/progress/extension/ProgressErrorCode.java
@@ -0,0 +1,17 @@
+package com.eduquest.backend.application.progress.extension;
+
+import com.eduquest.backend.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ProgressErrorCode implements ErrorCode {
+
+    FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
